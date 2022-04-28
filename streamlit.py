d2d6bf591a36af7db2fdde94ab9f65dd988f9769
cp268 <parksoh17@gmail.com>
2022-04-28 12:33:23 -0400

    update

--- streamlit.py
+++ streamlit.py
@@ -42,8 +42,8 @@ def main():
         #execute_recsys="",
     #)
 
-    st.session_state.get(model_computed=False)
-    st.session_state.get(execute_recsys=False)
+    st.session_state.get(model_computed)
+    st.session_state.get(execute_recsys()
 
     ingredients = st.text_input("Enter ingredients you would like to cook with")
     st.session_state.execute_recsys = st.button("Give me recommendations!")
